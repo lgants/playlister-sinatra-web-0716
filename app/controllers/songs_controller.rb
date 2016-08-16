33e8b0b3d09c11b6227a29aeebd2f3756cf1d13a
lgants <lgants@gmail.com>
2016-08-16 17:21:53 -0400

    playlister lab completed

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,4 +29,27 @@ class SongsController < ApplicationController
     redirect "/songs/#{@song.slug}"
   end
 
+  get "/songs/:slug/edit" do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  # {"_method"=>"patch",
+  #  "song"=>{"name"=>"Rolling In the Deep2"},
+  #  "artist"=>{"name"=>"Adele"},
+  #  "genre"=>["3"],
+  #  "splat"=>[],
+  #  "captures"=>["rolling-in-the-deep"],
+  #  "slug"=>"rolling-in-the-deep"}
+
+  patch "/songs/:slug" do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+    #flash[:message] = "Successfully updated song."
+    redirect("/songs/#{params[:slug]}")
+  end
+
+
 end
